fba0981df40c5c2c6f24dd6a7fac99a41b095b2c
byeongjun.kim <byeongjun.kim@emotion.co.kr>
2025-08-29 11:29:28 +0900

    포트폴리오 수정

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -1,55 +1,66 @@
-import React from 'react';
-import {Title, ProjectWrapper, ProjectCard, Tag} from '../styles/projectStyle'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import React from "react";
+import {
+  Title,
+  ProjectWrapper,
+  ProjectCard,
+  Tag,
+} from "../styles/projectStyle";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 // 예제 프로젝트 데이터 (실제 데이터는 props로 전달 가능)
-const projectData = [
-
-];
+const projectData = [];
 
 const githubRepos = [
- {
-  name: 'Project Xafe',
-  description: 'AED(자동 심장 충격기) 보관함 원격 모니터링 서비스 웹 애플리케이션입니다. 이 작업을 통해 Vue.js와 javascript에 대해 좀 심도있게 알게되었고, 양방향 데이터 바인딩에 대해 이해하게 되었습니다. 무엇보다 협업과 의사소통의 중요성을 몸소 실감하게 되었습니다.',
-  html_url: 'https://github.com/cowboy9408/Xafe-Vue.js-',
-  topics: ['JavaScript', 'Vue.js']
- },
- {
-  name: 'Project BJMCU',
-  description: 'MCU 모듈을 웹페이지와 연결하고 위치 정보를 기반으로 해당 모듈에서 센서 데이터가 수집되고 일정 시간마다 데이터가 정상적으로 갱신이 되는지 테스트하는 웹페이지이다. javascript를 이용해 서버를 구축의 경험과 Express.js와 미들웨어에 대해 이해하게 되었다. ',
-  html_url: 'https://github.com/cowboy9408/BJMCU-Node.js-',
-  topics: ['JavaScript', 'Node.js', 'Express.js', 'MongoDB']
- },
- {
-  name: 'Project Xchat',
-  description: '실시간 메신저 웹 애플리케이션이다. styled-component를 사용하고 있으며, 이 개발을 통해 코드 가독성과 유지보수성에 대해 많은 생각을 하게 되었다. 또 use 훅에 대해 좀더 깊이 있게 알게되었다.',
-  html_url: 'https://github.com/cowboy9408/Xchat-react.js-',
-  topics: ['JavaScript', 'React.js', 'styled-component',]
- },
- {
-  name: 'LGInnotek Operation',
-  description: 'LG이노텍 공식 웹페이지이다. 전체적인 구축은 아니었지만 리뉴얼 및 신규 페이지 구축과 Hcaptcha 도입으로 웹사이트에 보안에 기여했습니다.',
-  homepage: 'https://www.lginnotek.com/support/qna.do',
-  topics: ['JSP', 'HTML', 'Hcaptcha',]
- },
- {
-  name: 'OneGroveMall 프로젝트',
-  description:'본 프로젝트에서는 CMS 관리자 페이지 구축과 전체 API 연동 작업을 전담하였습니다. 관리자 기능(UI 포함), 데이터 처리, 인증/인가 로직, 비동기 통신 처리 등 핵심 기능을 React 기반으로 구현하였으며, 퍼블리셔와의 협업을 통해 UI 디자인을 연동하였습니다.',
-  homepage: 'https://admin-dev.onegrove.kr/',
-  html_url:'https://github.com/cowboy9408/Onegrove',
-  topics: ['React.js']
- },
- 
-
+  {
+    name: "Project Xafe",
+    description:
+      "AED(자동 심장 충격기) 보관함 원격 모니터링 서비스 웹 애플리케이션입니다. 이 작업을 통해 Vue.js와 javascript에 대해 좀 심도있게 알게되었고, 양방향 데이터 바인딩에 대해 이해하게 되었습니다. 무엇보다 협업과 의사소통의 중요성을 몸소 실감하게 되었습니다.",
+    html_url: "https://github.com/cowboy9408/Xafe-Vue.js-",
+    topics: ["JavaScript", "Vue.js"],
+  },
+  {
+    name: "Project BJMCU",
+    description:
+      "MCU 모듈을 웹페이지와 연결하고 위치 정보를 기반으로 해당 모듈에서 센서 데이터가 수집되고 일정 시간마다 데이터가 정상적으로 갱신이 되는지 테스트하는 웹페이지이다. javascript를 이용해 서버를 구축의 경험과 Express.js와 미들웨어에 대해 이해하게 되었다. ",
+    html_url: "https://github.com/cowboy9408/BJMCU-Node.js-",
+    topics: ["JavaScript", "Node.js", "Express.js", "MongoDB"],
+  },
+  {
+    name: "Project Xchat",
+    description:
+      "실시간 메신저 웹 애플리케이션이다. styled-component를 사용하고 있으며, 이 개발을 통해 코드 가독성과 유지보수성에 대해 많은 생각을 하게 되었다. 또 use 훅에 대해 좀더 깊이 있게 알게되었다.",
+    html_url: "https://github.com/cowboy9408/Xchat-react.js-",
+    topics: ["JavaScript", "React.js", "styled-component"],
+  },
+  {
+    name: "LGInnotek Operation",
+    description:
+      "LG이노텍 공식 웹페이지이다. 전체적인 구축은 아니었지만 리뉴얼 및 신규 페이지 구축과 Hcaptcha 도입으로 웹사이트에 보안에 기여했습니다.",
+    homepage: "https://www.lginnotek.com/support/qna.do",
+    topics: ["JSP", "HTML", "Hcaptcha"],
+  },
+  {
+    name: "OneGrove 관리자 CMS",
+    description:
+      "본 프로젝트에서는 CMS 관리자 페이지 구축과 전체 API 연동 작업을 전담하였습니다. 관리자 기능(UI 포함), 데이터 처리, 인증/인가 로직, 비동기 통신 처리 등 핵심 기능을 React 기반으로 구현하였으며, 퍼블리셔와의 협업을 통해 UI 디자인을 연동하였습니다.",
+    homepage: "https://admin-dev.onegrove.kr/",
+    html_url: "https://github.com/cowboy9408/Onegrove",
+    topics: ["React.js"],
+  },
+  {
+    name: "OneGroveMall",
+    description:
+      "본 프로젝트에서는 로그인 및 회원가입, 사용자 프로필 작업 , 해당 프로젝트 API 연동 작업을 전담하였습니다. 데이터 처리, 인증/인가 로직, 비동기 통신 처리 등 핵심 기능을 Next.js 기반으로 구현하였으며, 백엔드 및 디자이너와의 협업을 통해 연동 및 최적화 작업을 수행했습니다.",
+    homepage: "https://front-dev.onegrove.kr/",
+    html_url: "https://github.com/cowboy9408/onegrove-front",
+    topics: ["Next.js"],
+  },
 ];
-
-
-
 
 const Projects = () => {
   React.useEffect(() => {
-    AOS.init({ duration: 1000, easing: 'ease-in-out', once: true });
+    AOS.init({ duration: 1000, easing: "ease-in-out", once: true });
   }, []);
 
   return (
@@ -61,7 +72,12 @@ const Projects = () => {
             <h3>{project.name}</h3>
             <p>{project.descr}</p>
             {project.demo && (
-              <a className="highlight-link" href={project.demo} target="_blank" rel="noreferrer">
+              <a
+                className="highlight-link"
+                href={project.demo}
+                target="_blank"
+                rel="noreferrer"
+              >
                 Demo
               </a>
             )}
@@ -78,16 +94,26 @@ const Projects = () => {
             <h3>{repo.name}</h3>
             <p>{repo.description}</p>
             {repo.homepage && (
-  <a className="highlight-link" href={repo.homepage} target="_blank" rel="noreferrer">
-    Demo
-  </a>
-)}
+              <a
+                className="highlight-link"
+                href={repo.homepage}
+                target="_blank"
+                rel="noreferrer"
+              >
+                Demo
+              </a>
+            )}
 
-{repo.html_url && (
-  <a className="highlight-link" href={repo.html_url} target="_blank" rel="noreferrer">
-    Github
-  </a>
-)}
+            {repo.html_url && (
+              <a
+                className="highlight-link"
+                href={repo.html_url}
+                target="_blank"
+                rel="noreferrer"
+              >
+                Github
+              </a>
+            )}
             <div>
               {repo.topics?.map((topic, i) => (
                 <Tag key={i}>{topic}</Tag>
@@ -101,4 +127,3 @@ const Projects = () => {
 };
 
 export default Projects;
-
--- my-portfolio/src/components/Timeline.jsx
+++ my-portfolio/src/components/Timeline.jsx
@@ -1,65 +1,72 @@
-import React from 'react';
-import styled from 'styled-components';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
+import React from "react";
+import styled from "styled-components";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const timelineData = [
   {
-    title: '국비 지원 교육을 시작으로....',
-    date: '2022-05',
-    subtitle: '어깨 너머로만 보던 IT 분야... 직접 공부하고 배우면서 꿈을 키우게됩니다.',
-    tags: ['Turning Point'],
-    titleUrl: '#',
+    title: "국비 지원 교육을 시작으로....",
+    date: "2022-05",
+    subtitle:
+      "어깨 너머로만 보던 IT 분야... 직접 공부하고 배우면서 꿈을 키우게됩니다.",
+    tags: ["Turning Point"],
+    titleUrl: "#",
   },
   {
-    title: 'IT 개발자로써의 첫 커리어',
-    date: '2022-11',
-    subtitle: 'IT 개발자로서 첫 취직을 해 꿈을 향해 달려가 볼 수 있는 기회를 얻었습니다.',
-    tags: ['First Career'],
-    titleUrl: '#',
+    title: "IT 개발자로써의 첫 커리어",
+    date: "2022-11",
+    subtitle:
+      "IT 개발자로서 첫 취직을 해 꿈을 향해 달려가 볼 수 있는 기회를 얻었습니다.",
+    tags: ["First Career"],
+    titleUrl: "#",
   },
   {
-    title: 'IT 개발자로서의 첫 실무',
-    date: '2023-01',
-    subtitle: '본격 커리어의 시작! 처음으로 팀원들과 업무에 대해 회의를 진행하면서 협업을 진행해보았습니다. 이름하여 Xafe 프로젝트!',
-    tags: ['Vue.js', 'First Project'],
-    titleUrl: '#',
+    title: "IT 개발자로서의 첫 실무",
+    date: "2023-01",
+    subtitle:
+      "본격 커리어의 시작! 처음으로 팀원들과 업무에 대해 회의를 진행하면서 협업을 진행해보았습니다. 이름하여 Xafe 프로젝트!",
+    tags: ["Vue.js", "First Project"],
+    titleUrl: "#",
   },
   {
-    title: '팀의 업무 효율에 지대한 기여를 하다.',
-    date: '2024-03',
-    subtitle: '저의 기획으로 탄생한 BJMCU 프로젝트! 모듈 테스트 간소화를 통해 팀 업무에 기여했습니다.',
-    tags: ['Node.js','Express.js', 'Second Project'],
-    titleUrl: '#',
+    title: "팀의 업무 효율에 지대한 기여를 하다.",
+    date: "2024-03",
+    subtitle:
+      "저의 기획으로 탄생한 BJMCU 프로젝트! 모듈 테스트 간소화를 통해 팀 업무에 기여했습니다.",
+    tags: ["Node.js", "Express.js", "Second Project"],
+    titleUrl: "#",
   },
   {
-    title: '한걸음 더 나아가다.',
-    date: '2024-10',
-    subtitle: '실시간 메신저 웹 애플리케이션을 개발을 진행하게 되면서 디자인도 맡아서 기획해 볼 수 있었고, React.js라는 언어와 저에 대해 보다 심층적으로 접근해 볼 수 있었습니다.',
-    tags: ['React.js', 'Third Project'],
-    titleUrl: '#',
+    title: "한걸음 더 나아가다.",
+    date: "2024-10",
+    subtitle:
+      "실시간 메신저 웹 애플리케이션을 개발을 진행하게 되면서 디자인도 맡아서 기획해 볼 수 있었고, React.js라는 언어와 저에 대해 보다 심층적으로 접근해 볼 수 있었습니다.",
+    tags: ["React.js", "Third Project"],
+    titleUrl: "#",
   },
   {
-    title: '새로운 곳에서 새로운 도전!',
-    date: '2025-03',
-    subtitle: '새로운 곳에서 더 발전하고 도약하는 프론트엔드 개발자가 되기 위해 새로운 도전을 시도했습니다. 두번째 터전에서!',
-    tags: ['Second Career'],
-    titleUrl: '#',
+    title: "새로운 곳에서 새로운 도전!",
+    date: "2025-03",
+    subtitle:
+      "새로운 곳에서 더 발전하고 도약하는 프론트엔드 개발자가 되기 위해 새로운 도전을 시도했습니다. 두번째 터전에서!",
+    tags: ["Second Career"],
+    titleUrl: "#",
   },
   {
-    title: '첫 유지운영 업무! 보안에 기여하다!',
-    date: '2025-03',
-    subtitle: 'LG이노텍과 LG복지몰에 Hcaptcha를 최초로 도입해 사이트 보안에 기여했습니다.',
-    tags: ['JSP','Hcaptcha'],
-    titleUrl: '#',
+    title: "첫 유지운영 업무! 보안에 기여하다!",
+    date: "2025-03",
+    subtitle:
+      "LG이노텍과 LG복지몰에 Hcaptcha를 최초로 도입해 사이트 보안에 기여했습니다.",
+    tags: ["JSP", "Hcaptcha"],
+    titleUrl: "#",
   },
   {
-    title: '대형 구축 프로젝트를 성공적으로 이끌다!',
-    date: '2025-07',
-    subtitle: '원그로브몰 CMS 구축 프로젝트의 메인 프론트엔드 개발자로써 서비스 오픈까지 성공적으로 주도했습니다!.',
-    tags: ['React.js'],
-    titleUrl: '#',
+    title: "대형 구축 프로젝트를 성공적으로 이끌다!",
+    date: "2025-07",
+    subtitle:
+      "원그로브몰 구축 프로젝트의 메인 프론트엔드 개발자로써 서비스 오픈까지 성공적으로 주도했습니다!.",
+    tags: ["React.js, Next.js"],
+    titleUrl: "#",
   },
   // 추가 이벤트...
 ];
@@ -91,7 +98,7 @@ const TimelineList = styled.ul`
   position: relative;
   margin: 0;
   &::before {
-    content: '';
+    content: "";
     position: absolute;
     top: 0;
     bottom: 0;
@@ -107,41 +114,41 @@ const TimelineItem = styled.li`
   width: 50%;
   padding: 20px;
   box-sizing: border-box;
-  text-align: ${({ side }) => (side === 'left' ? 'right' : 'left')};
-  left: ${({ side }) => (side === 'left' ? '0' : '50%')};
-  transform: ${({ side }) => (side === 'left' ? 'translateX(-5%)' : 'translateX(5%)')};
+  text-align: ${({ side }) => (side === "left" ? "right" : "left")};
+  left: ${({ side }) => (side === "left" ? "0" : "50%")};
+  transform: ${({ side }) =>
+    side === "left" ? "translateX(-5%)" : "translateX(5%)"};
 
   &::after {
-    content: '';
+    content: "";
     position: absolute;
     top: 20px;
     width: 10px;
     height: 10px;
     background: #e74c3c;
     border-radius: 50%;
-    left: ${({ side }) => (side === 'left' ? 'calc(100% + 4px)' : '-14px')};
+    left: ${({ side }) => (side === "left" ? "calc(100% + 4px)" : "-14px")};
   }
 `;
 
 const EventContent = styled.div`
- display: flex;
+  display: flex;
   flex-direction: column;
   justify-content: center;
   width: 280px;
   border-radius: 20px;
   background: linear-gradient(145deg, #4a4a4a, #222);
-  box-shadow: 
-    6px 6px 12px rgba(0, 0, 0, 0.3),  /* 바깥 그림자를 줄이고 투명도 조정 */
-    -6px -6px 12px rgba(255, 255, 255, 0.05), /* 밝은 양각 그림자 최소화 */
-    inset 3px 3px 5px rgba(0, 0, 0, 0.4), /* 내부 어두운 음영 */
-    inset -3px -3px 5px rgba(255, 255, 255, 0.1); /* 내부 밝은 음영 */
+  box-shadow: 6px 6px 12px rgba(0, 0, 0, 0.3),
+    /* 바깥 그림자를 줄이고 투명도 조정 */ -6px -6px 12px
+      rgba(255, 255, 255, 0.05),
+    /* 밝은 양각 그림자 최소화 */ inset 3px 3px 5px rgba(0, 0, 0, 0.4),
+    /* 내부 어두운 음영 */ inset -3px -3px 5px rgba(255, 255, 255, 0.1); /* 내부 밝은 음영 */
   padding: 20px;
   transition: all 0.3s ease;
-  margin: ${({ side }) => (side === 'left' ? '0 0 0 auto' : '0 auto 0 0')};
+  margin: ${({ side }) => (side === "left" ? "0 0 0 auto" : "0 auto 0 0")};
 
   &:hover {
-    box-shadow: 
-      8px 8px 16px rgba(0, 0, 0, 0.3),
+    box-shadow: 8px 8px 16px rgba(0, 0, 0, 0.3),
       -8px -8px 16px rgba(255, 255, 255, 0.05),
       inset 4px 4px 6px rgba(0, 0, 0, 0.5),
       inset -4px -4px 6px rgba(255, 255, 255, 0.1);
@@ -149,7 +156,7 @@ const EventContent = styled.div`
   }
 
   &::before {
-    content: '';
+    content: "";
     position: absolute;
     top: 0;
     left: 0;
@@ -168,8 +175,8 @@ const EventContent = styled.div`
 
 const EventTitle = styled.a`
   color: #fff; /* text color */
-  position:relative;
-  bottom:15px;
+  position: relative;
+  bottom: 15px;
   font-size: 1.25em;
   font-weight: bold;
   text-decoration: none;
@@ -183,33 +190,34 @@ const EventDate = styled.time`
   display: block;
   margin-bottom: 10px;
   color: #e74c3c;
-  position:relative;
-  bottom:11px;
+  position: relative;
+  bottom: 11px;
 `;
 
 const EventDescription = styled.p`
   margin: 0;
   font-size: 1rem;
   line-height: 1.5;
-  position:relative;
-  bottom:11px;
+  position: relative;
+  bottom: 11px;
 `;
 
 const Tag = styled.span`
   background: #555;
-  position:relative;
-  top:5px;
+  position: relative;
+  top: 5px;
   color: #fff;
   padding: 5px;
   border-radius: 5px;
   margin-right: 5px;
   font-size: 0.875rem;
-  box-shadow: inset -2px -2px 5px rgba(0, 0, 0, 0.5), inset 2px 2px 5px rgba(255, 255, 255, 0.2);
+  box-shadow: inset -2px -2px 5px rgba(0, 0, 0, 0.5),
+    inset 2px 2px 5px rgba(255, 255, 255, 0.2);
 `;
 
 const Timeline = () => {
   React.useEffect(() => {
-    AOS.init({ duration: 1000, easing: 'ease-in-out', once: true });
+    AOS.init({ duration: 1000, easing: "ease-in-out", once: true });
   }, []);
 
   return (
@@ -218,12 +226,12 @@ const Timeline = () => {
       <SectionSubtitle>저의 발자취를 간략하게 보여드립니다!</SectionSubtitle>
       <TimelineList>
         {timelineData.map((event, index) => (
-          <TimelineItem key={index} side={index % 2 === 0 ? 'left' : 'right'}>
+          <TimelineItem key={index} side={index % 2 === 0 ? "left" : "right"}>
             <EventContent
-              side={index % 2 === 0 ? 'left' : 'right'}
-              data-aos={index % 2 === 0 ? 'fade-right' : 'fade-left'}
+              side={index % 2 === 0 ? "left" : "right"}
+              data-aos={index % 2 === 0 ? "fade-right" : "fade-left"}
             >
-              <EventTitle  target="_blank" rel="noreferrer">
+              <EventTitle target="_blank" rel="noreferrer">
                 {event.title}
               </EventTitle>
               <EventDate>{event.date}</EventDate>
